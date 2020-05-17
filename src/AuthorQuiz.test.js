496b68c42037013e4442e760429997976da1e8f6
Piyush Bajaj <45912468+piyush-bajaj@users.noreply.github.com>
2020-05-17 18:37:36 +0530

    simple test case

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import { render } from '@testing-library/react';
 import AuthorQuiz from './AuthorQuiz';
 
-test( 'renders learn react link', () => {
-	const { getByText } = render( <AuthorQuiz /> );
-	const linkElement = getByText( /Author Quiz/i );
-	expect( linkElement ).toBeInTheDocument();
+const state = {
+	turnData: {
+		books: ['IT', 'Romeo and Juliet', 'The Shining', 'Harry Potter and Socerers stone'],
+		author: {
+			name: "William Shakespeare",
+			imageUrl: "images/authors/williamshakespeare.jpg",
+			imageSource: "Wikimedia Commons",
+			books: ["Romeo and Juliet", "Hamlet", "Macbeth"]
+		}
+	},
+	highlight: "none"
+}
+
+describe( 'Author Quiz', () => {
+	it( "renders without crashing", () => {
+		const div = document.createElement( "div" );
+		ReactDOM.render( <AuthorQuiz { ...state } onAnswerSelected={ () => { } } />, div );
+	} );
 } );
+
+
+// test( 'renders learn react link', () => {
+// 	const { getByText } = render( <AuthorQuiz /> );
+// 	const linkElement = getByText( /Author Quiz/i );
+// 	expect( linkElement ).toBeInTheDocument();
+// } );
