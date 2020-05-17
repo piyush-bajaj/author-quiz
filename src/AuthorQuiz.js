eb0e95d07327abd10092b5a7bbb9ebc3e8391d28
Piyush Bajaj <45912468+piyush-bajaj@users.noreply.github.com>
2020-05-17 12:27:41 +0530

    multiple options, author photos

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -6,7 +6,7 @@ function Hero() {
 	return ( <div className="row">
 		<div className="jumbotron col-10 offset-1">
 			<h1>Author Quiz</h1>
-			<p>Select the book return by the author shown</p>
+			<p>Select the book written by the author shown</p>
 		</div>
 	</div> );
 }
