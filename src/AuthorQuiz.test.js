0bde4f2df3b75e3c7f7c8391ff4891078beab365
Piyush Bajaj <45912468+piyush-bajaj@users.noreply.github.com>
2020-05-31 15:11:33 +0530

    adding some tests with enzyme

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import { render } from '@testing-library/react';
 import AuthorQuiz from './AuthorQuiz';
+import Enzyme, { mount, shallow, render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure( { adapter: new Adapter() } );
 
 const state = {
 	turnData: {
@@ -20,6 +22,69 @@ describe( 'Author Quiz', () => {
 	it( "renders without crashing", () => {
 		const div = document.createElement( "div" );
 		ReactDOM.render( <AuthorQuiz { ...state } onAnswerSelected={ () => { } } />, div );
+	} );
+
+	describe( "when no answer is selected", () => {
+		let wrapper;
+		beforeAll( () => {
+			wrapper = mount( <AuthorQuiz { ...state } onAnswerSelected={ () => { } } /> );
+		} );
+
+		it( "should have white background color", () => {
+			expect( wrapper.find( "div.row.turn .authorimage" ).props().src ).toBeDefined();
+			expect( wrapper.find( "div.row.turn" ).props().style.backgroundColor ).toBe( 'white' );
+		} );
+	} );
+
+	describe( "when correct answer is selected", () => {
+		let wrapper;
+		beforeAll( () => {
+			wrapper = mount( <AuthorQuiz { ...( Object.assign( {}, state, { highlight: "correct" } ) ) } onAnswerSelected={ () => { } } /> );
+		} );
+
+		it( "should have green background color", () => {
+			expect( wrapper.find( "div.row.turn .authorimage" ).props().src ).toBeDefined();
+			expect( wrapper.find( "div.row.turn" ).props().style.backgroundColor ).toBe( 'green' );
+		} );
+
+	} );
+
+	describe( "when incorrect answer is selected", () => {
+		let wrapper;
+		beforeAll( () => {
+			wrapper = mount( <AuthorQuiz { ...( Object.assign( {}, state, { highlight: "wrong" } ) ) } onAnswerSelected={ () => { } } /> );
+		} );
+
+		it( "should have red background color", () => {
+			expect( wrapper.find( "div.row.turn .authorimage" ).props().src ).toBeDefined();
+			expect( wrapper.find( "div.row.turn" ).props().style.backgroundColor ).toBe( 'red' );
+		} );
+
+	} );
+
+	describe( "when first answer is selected", () => {
+		let wrapper;
+		const handleAnswerSelected = jest.fn();
+		beforeAll( () => {
+			wrapper = mount( <AuthorQuiz { ...( Object.assign( {}, state, { highlight: "wrong" } ) ) } onAnswerSelected={ handleAnswerSelected } /> );
+			wrapper.find( ".answer" ).first().simulate( "click" );
+		} );
+
+		it( "onAnswerSelected should be called", () => {
+			expect( handleAnswerSelected ).toBeCalled();
+			expect( handleAnswerSelected ).toHaveBeenCalled();
+
+			// expect( wrapper.find( "div.row.turn" ).props().style.backgroundColor ).toBe( 'red' );
+		} );
+
+		it( "should receive IT", () => {
+			// expect( handleAnswerSelected ).toBeCalled();
+			// expect( handleAnswerSelected ).toHaveBeenCalled();
+			expect( handleAnswerSelected ).toHaveBeenCalledWith( "IT" );
+
+
+			// expect( wrapper.find( "div.row.turn" ).props().style.backgroundColor ).toBe( 'red' );
+		} );
 	} );
 } );
 
